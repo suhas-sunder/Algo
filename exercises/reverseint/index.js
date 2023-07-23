a3b51c225d6a56d0b8371289e574d544f07dc797
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-07-22 23:42:49 -0400

    Feat: reverseint.
    Add solution to reverseint.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Reverse string array and return number
+function backToNum(strArr) {
+  let num = parseInt(strArr.reverse().join(""));
+  return num;
+}
+
+function reverseInt(n) {
+  // Convert num to string array
+  let strNum = n.toString().split("");
+
+  //Handle negative values
+  if (strNum.includes("-")) {
+    strNum.shift("-");
+    return backToNum(strNum) * -1;
+  } else {
+    return backToNum(strNum);
+  }
+}
 
 module.exports = reverseInt;
