4185bc1d72a548768c7ffb2083b4940f1e9d2e7b
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-07-24 11:51:45 -0400

    feat: maxChar solution
    Add working solution to maxChar problem.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // Here is the first solution that comes to mind.
+  // Split string into an array
+  let strArr = str.split("");
+  // Convert array into a set to filter out only unique values
+  let strSet = new Set(strArr);
+  // Check how many times each unique char appears in string arr
+  let mostRepeatedChar;
+  let prevCharRepetition = 0;
+
+  strSet.forEach((uniqueChar) => {
+    let charRepetition = 0;
+    strArr.forEach((char) => {
+      if (char === uniqueChar) {
+        charRepetition++;
+      }
+    });
+
+    // Keep track of most repeated character
+    if (charRepetition > prevCharRepetition) {
+      prevCharRepetition = charRepetition;
+      mostRepeatedChar = uniqueChar;
+    }
+  });
+  
+  return mostRepeatedChar;
+}
+
 
 module.exports = maxChar;
