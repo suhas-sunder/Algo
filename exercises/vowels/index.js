55a962d689894d908d9e797c9f56fad77fdff6ab
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-08-02 18:08:47 -0400

    feat: solution for vowels
    -Add working solutions for vowels problem.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// First
+// function vowels(str) {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   let totalVowels = 0;
+//   str
+//     .toLowerCase()
+//     .split("")
+//     .forEach((char) => {
+//       if (vowels.includes(char)) totalVowels++;
+//     });
+
+//   return totalVowels;
+// }
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
