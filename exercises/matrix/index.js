cf98ed82bde46622772c29997e7bb3cbb36905b1
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-08-02 23:16:25 -0400

    feat: solve matrix problem
    -Add working solution to matrix probelm.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,43 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let top = 0;
+    let right = n - 1;
+    let bottom = n - 1;
+    let left = 0;
+    // Create an empty array with n sub arrays that are unique. new Array(n).fill([]) doesn't work since all sub arrays have the same pointer.
+    let spiralMatrix = new Array(n).fill().map(() => new Array().fill());
+    let count = 1;
+
+    while(count <= n * n) {       
+        // Sweep left to right on top
+        for(let i = left; i <=  right; i++) {
+            spiralMatrix[top][i] = count++;
+        }
+        top++;
+
+        // Sweep top to bottom on right
+        for(let i = top; i <= bottom; i++) {
+            spiralMatrix[i][right] = count++;
+        }
+        right--;
+
+        // Sweep right to left on bottom
+        for(let i = right; i >=  left; i--) {
+            spiralMatrix[bottom][i] = count++;
+        }
+        bottom--;
+
+        // Sweep bottom to top on left
+        for(let i = bottom; i >= top; i--) {
+            spiralMatrix[i][left] = count++;
+        }
+        left++;
+
+    }
+    
+    return spiralMatrix;
+}
 
 module.exports = matrix;
